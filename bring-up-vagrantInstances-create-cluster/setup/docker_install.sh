f58c3815a59c9e8dee1ab66067ebd4902cb0815a
Savio Mathew <savifrnds@gmail.com>
2018-08-30 17:16:00 +0530

    removing Vagrant k8s cluster setup

--- bring-up-vagrantInstances-create-cluster/setup/docker_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -x
-
-#Adding docker repo
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-#Installing and starting docker
-yum -y install docker-ce;systemctl enable docker;systemctl start docker
